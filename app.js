44e3d0ab457c375484a73533edda7842e77e129b
Juan Tomas Castellon <juan.castellon@ultertechnologies.com>
2020-04-24 23:36:14 -0500

    [feat]: added visualization for the api

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 var taskRouter = require('./routes/task');
 
 var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
